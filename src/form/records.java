81ed7bdd966b634b658e2c14666868a04fff0df6
ram4209 <alfaroabram44@gmail.com>
2025-06-15 09:14:57 +0800

    change record to records

--- src/form/records.java
+++ src/form/records.java
@@ -10,13 +10,13 @@ import javax.swing.*;
 import java.lang.*;
 import java.sql.Date;
 import java.sql.*;
-public class record extends javax.swing.JFrame {
+public class records extends javax.swing.JFrame {
 
     private Connection con;
     private Statement st;
     private ResultSet rs;
     int i = 0;
-    public record() {
+    public records() {
         try {
             con = DriverManager.getConnection("jdbc:mysql://localhost:3306/im-group1", "root", "password");
             st = con.createStatement();
@@ -674,20 +674,20 @@ public class record extends javax.swing.JFrame {
                 }
             }
         } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(record.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(records.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(record.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(records.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(record.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(records.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(record.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(records.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
         //</editor-fold>
 
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
-                new record().setVisible(true);
+                new records().setVisible(true);
             }
         });
     }
